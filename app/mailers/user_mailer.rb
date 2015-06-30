d13621f7e2ffe222ef7ee5f99dd321b5056dacd9
alejoescobar <alejoescobac@gmail.com>
2015-06-30 13:19:08 -0500

    Add initial question delivery mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < BaseMandrillMailer
   def daily_question(user_id)
     user = User.find(user_id)
-    subject = "¡Bienvenido a Vuelteros!"
+    subject = "Pregunta #{Question.first.id}"
     merge_vars = {
       "NAME" => user.name,
-      "NEW_URL" => new_service_url
     }
     body = mandrill_template("cienpreguntas_questions", merge_vars)
 
