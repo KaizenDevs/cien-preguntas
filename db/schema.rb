3a6ee9ce5066720dea61421299a9e007bedf98f5
alejoescobar <alejoescobac@gmail.com>
2015-06-30 12:50:59 -0500

    Fix model association commands

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20150630172756) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +45,14 @@ ActiveRecord::Schema.define(version: 20150630172756) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+<<<<<<< HEAD
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+=======
+    t.string   "name"
+    t.string   "lastname"
+>>>>>>> Fix model association commands
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
