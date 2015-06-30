dc2d5e61d1f206ca3e67d6ee03b378e1a4d3269e
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-06-30 13:13:48 -0500

    Add notices and alerts to home page

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  answer      :string
+#  question_id :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
