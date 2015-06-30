2ab9b02557e7ca708254b02f84fe1d2c1ee4d586
alejoescobar <alejoescobac@gmail.com>
2015-06-30 12:50:59 -0500

    Fix model association commands

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  has_many :answers
+  has_many :questions, through: :answers
 end
