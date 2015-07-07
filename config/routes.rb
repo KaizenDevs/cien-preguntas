9ea31263458e02a6f7d67ebb1f773f90b2db5e21
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-07 13:19:20 -0500

    Add routes for omniauth devise callback | Create Omniauths controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers
   end
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
