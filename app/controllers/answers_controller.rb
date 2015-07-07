1d23e4946a53297b0a32f6f053fe0e51ff6edfb2
German Hernandez <muygrafico@gmail.com>
2015-07-07 10:02:57 -0500

    add general styles | answer controller created | add public state column to answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,25 @@
 class AnswersController < ApplicationController
   def new
-    @question = Question.find(params[:question_id]).question
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
   end
-
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.user_id = 1
+    @answer.question_id = params[:question_id]
+    @answer.save
+    redirect_to [@question, @answer]
+  end
   def edit
   end
-
   def show
+    @answer = Answer.find(params[:id])
   end
-
   def index
+  end
+  private
+  def answer_params
+    params.require(:answer).permit(:answer, :public)
   end
 end
