a2b4e57a3103e9af6851a93034ea0d795b8d8841
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-07 13:29:52 -0500

    Add actio to copie the facebook email if available

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,15 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 
