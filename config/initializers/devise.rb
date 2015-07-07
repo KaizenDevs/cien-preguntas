7599fb5e100c6a8f95bd294664fcb5e86f6c5d63
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-07 13:57:35 -0500

    Declare twitter provider for omni-auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,4 +263,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_secret"]
+  config.omniauth :twitter, ENV["twitter_app_id"], ENV["twitter_secret"]
 end
