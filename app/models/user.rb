a483e9d6d48fd8217aad5c4fb62a49d375f3fc51
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-07 14:01:32 -0500

    Make User model omniauthable for twitter provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
   has_many :answers
   has_many :questions, through: :answers
