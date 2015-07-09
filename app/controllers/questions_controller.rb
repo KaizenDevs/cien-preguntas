41671bd178d8f3c3bdc4d0f0c57fba14c855f840
alejoescobar <alejoescobac@gmail.com>
2015-07-08 19:51:32 -0500

    Finish question actions and add index and new views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,20 @@
 class QuestionsController < ApplicationController
+
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new()
   end
 
   def create
+    @question = Question.new()
   end
+
+  private
+  def question_params
+    params.require(:question).permit(:question)
+  end
+
 end
