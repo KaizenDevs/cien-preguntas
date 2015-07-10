86834b36d060c606d8371dbe79c795d55f846911
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-10 10:24:49 -0500

    Add edit action with conditional for facebook users | Add strong parameters for devise registrtations controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+	before_filter :authenticate_user!
+	include ApplicationHelper
+
+	def edit 
+		@user = current_user
+		if facebook_user?
+			@temp = true
+			render "edit_facebook_user"
+		else
+			render "edit"
+		end
+	end
 end
