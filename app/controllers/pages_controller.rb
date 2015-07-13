331d8950145aaccd2a17d09323f0540df3daeec0
alejoescobar <alejoescobac@gmail.com>
2015-07-13 11:44:19 -0500

    Add permissions method for admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,21 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:profile]
+  before_action :admin_only, only: [:metrics]
 
 	def home
 	end
 
   def profile
     @user = current_user
+  end
+
+  def metrics
+  end
+
+  private
+  def admin_only
+    unless current_user.admin?
+      redirect_to root_path, :alert => "Acceso denegado, no posee permisos como administrador"
+    end
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,46 @@
 class QuestionsController < ApplicationController
+  before_action :admin_only
 
   def index
-    @questions = Question.all
+    @questions = Question.all.sort
   end
 
   def new
     @question = Question.new()
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
   def create
     @question = Question.new(question_params)
+    if Question.last.nil?
+      @question.id = 1
+    else
+      @question.id = Question.last.id + 1
+    end
 
     if @question.save
       redirect_to questions_path
     else
       render 'new'
     end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path, notice: "Se ha editado la pregunta con éxito."
+    else
+      render 'edit', alert: "Hubo un error y no se pudo editar la pregunta."
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   def import
@@ -29,7 +54,13 @@ class QuestionsController < ApplicationController
 
   private
   def question_params
-    params.require(:question).permit(:question)
+    params.require(:question).permit(:question, :id)
+  end
+
+  def admin_only
+    unless current_user.admin?
+      redirect_to root_path, :alert => "Acceso denegado, no posee permisos como administrador"
+    end
   end
 
 end
