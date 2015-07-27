287ddbc47a7815abd183e22f6ef4e0be26de595d
alejoescobar <alejoescobac@gmail.com>
2015-07-27 11:23:32 -0500

    Fix action mailer default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'salty-meadow-1965.heroku.com' }
+
   config.action_mailer.smtp_settings = {
       address: ENV.fetch("SMTP_ADDRESS"),
       authentication: :plain,
